5b79022e25460aa85f81475fc0ba12cc5f837fe1
mpascualach <miguelpacheson@hotmail.es>
2019-03-22 12:13:20 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// const vid = document.getElementById("love-death");
+
+// const playbutton = document.querySelector('.playbutton');
+
+// playbutton.addEventListener('click', function (e) {
+//     if(document.querySelector('video').playing){ 
+//         vid.pause();
+//         playbutton.classList.remove('hidden');
+//         $("#social-media-links").fadeIn();
+//     }else{
+//         vid.play(); 
+//         playbutton.classList.add('hidden');
+//         $("#social-media-links").fadeOut();
+//     }
+// });
+
+// Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+//     get: function(){
+//         return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+//     }
+// })
+
+// function showLinks() {
+//     $("#social-media-links").fadeIn();
+// }
